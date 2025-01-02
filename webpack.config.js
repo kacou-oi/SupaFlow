47b8629d6097a8f2a079f579eb4cda1cd0924100
Ange Kacou Oi <ange.kacou.oi@gmail.com>
2025-01-02 05:44:29 +0000

    clear

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  entry: './workers/supabase-worker.js',
-  output: {
-    filename: 'index.js',
-    path: __dirname + '/dist',
-  },
-  mode: 'production',
-  target: 'webworker',
-};
