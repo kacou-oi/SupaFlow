57d1aa1c049de3419e29cc2a6fd6b36434fd40d3
Ange Kacou Oi <ange.kacou.oi@gmail.com>
2025-01-03 03:57:34 +0000

    for install only

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2';
+
 const supabaseUrl = 'https://pfzxcukixwflfxxtxmvp.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmenhjdWtpeHdmbGZ4eHR4bXZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU3MjM5ODYsImV4cCI6MjA1MTI5OTk4Nn0.IeWlfZQvCp7HMBNoPflJXvFi68ZvXribMzrivpZnMcU';
-
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2';
 
 export function getSupabaseClient() {
   return createClient(supabaseUrl, supabaseKey);
